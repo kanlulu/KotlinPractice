a5f3240747f8136a0a73b94c1dd5e5a39495cff5
kanlulu <kanlulu@xiaoyouzi.com>
2020-02-03 16:40:51 +0800

    kotlin中特有的类 data数据类  sealed密闭类

---
+++ app/src/main/java/com/kanlulu/kotlin01/data/TestData.kt
@@ -0,0 +1,18 @@
+package com.kanlulu.kotlin01.data
+
+/**
+ * author:kanlulu
+ * data  :2020-02-03 16:27
+ *
+ **/
+data class TestData(
+    var string: String,
+    var age:Int)
+
+
+/**
+ * 密闭类
+ */
+sealed class TestSealed(){
+
+}
